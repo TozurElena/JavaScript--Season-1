19196fdcfe25ee2f29d671dd17ec38e6722eb351
Elena Kozyreva <elena.kozyreva.prof@gmail.ru>
2022-03-04 16:03:04 +0100

    terminer 9-1, 9-2

--- 02.The-Hill/08.Javascript/01.Season-1/09-misc/01-waving-text/script.js
+++ 02.The-Hill/08.Javascript/01.Season-1/09-misc/01-waving-text/script.js
@@ -3,6 +3,26 @@
 
 (() => {
 
-    // your code here
+    let textAll = document.getElementById('target');
+    let letters = textAll.innerText;
+    
+    letters = letters.split('');
+    textAll.innerText = '';
+
+    const randomSyze = () =>  Math.floor(Math.random() * 5) + 1;
+
+    letters.forEach((l, a) => {
+        let span = document.createElement('span');
+        span.innerText = l;
+        for (let i = 0; i < letters.length; i++) {
+            span.style.fontSize = randomSyze() + "rem";
+        }
+        textAll.appendChild(span);
+
+    });   
+                
+
 
 })();
+
+// https://www.sliderrevolution.com/resources/css-text-animation/
--- 02.The-Hill/08.Javascript/01.Season-1/09-misc/02-typewriter-effect/script.js
+++ 02.The-Hill/08.Javascript/01.Season-1/09-misc/02-typewriter-effect/script.js
@@ -3,6 +3,22 @@
 
 (() => {
 
-    // your code here
+   
+    let txt = document.getElementById('target').innerHTML; /* The text */
+    
+    const random = () =>  Math.floor(Math.random() * 150) + 30; /* The speed/duration of the effect in milliseconds */
+    
+    document.getElementById('target').innerHTML = '';
+
+    let i = 0;
+    function typeWriter() {
+        if (i < txt.length) {
+            document.getElementById('target').innerHTML += txt.charAt(i);
+            i++;
+            setTimeout(typeWriter, random());
+        }
+    }
+
+    typeWriter();
 
 })();
